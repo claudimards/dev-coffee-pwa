be1408e09ce5cd145e69d9577a826813489c0ca2
Claudimar Bezerra <claudimards@gmail.com>
2021-01-15 12:21:05 -0300

    modificado regras de cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,12 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
   '/fallback.html',
   '/css/tic-tac-toe.css',
   '/js/tic-tac-toe.js'
@@ -13,10 +20,22 @@ self.addEventListener('install', installEvent => {
   )
 })
 
+self.addEventListener('activate', evt => {
+  //console.log('service worker has been activated')
+  evt.waitUntil(
+      caches.keys().then(keys => {
+          return Promise.all(keys
+              .filter(key => key !== staticDevCoffee)
+              .map(key => caches.delete(key))
+          )
+      })
+  )
+})
+
 self.addEventListener('fetch', fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request)
-    })
+    }).catch(() => caches.match('/fallback.html'))
   )
 })
