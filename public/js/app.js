0c8d4033196ac5b686387edbe3d22b92130c41c7
Claudimar Bezerra <claudimards@gmail.com>
2021-01-18 15:57:02 -0300

    Adicionado banner install app

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ const coffees = [
   { name: " Vitae", image: "images/coffee1.jpg" },
   { name: "Inventore", image: "images/coffee1.jpg" },
   { name: "Veritatis", image: "images/coffee2.jpg" },
-  { name: "Accusantium", image: "images/coffee3.jpg" },
+  { name: "Accusantium", image: "images/coffee3.jpg" }
 ]
 
 const showCoffees = () => {
@@ -31,7 +31,39 @@ document.addEventListener("DOMContentLoaded", showCoffees)
 if('serviceWorker' in navigator){
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('serviceWorker.js')
-    .then(res => console.log('Service Worker registered'))
-    .catch(error => console.log('Service Worker not registered'))
   })
 }
+
+const divInstall = document.querySelector('.div-install')
+const butInstall = document.querySelector('#btn-install')
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Stash the event so it can be triggered later.
+  event.preventDefault()
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container
+  divInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  promptEvent.userChoice.then((result) => {
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+  });
+});
+
+const btnClose = document.querySelector('#close-install')
+btnClose.addEventListener('click', event => {
+  divInstall.classList.toggle('hidden', true);
+})
